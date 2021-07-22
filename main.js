c82c354bf067e09bef32b23852bccc14666273b2
Srey1234 <74773452+Srey1234@users.noreply.github.com>
2021-07-22 20:33:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,30 +9,43 @@ function preload()
 
 function setup()
 {
-    canvas = createCanvas( 640 , 420 );
+    canvas = createCanvas( 380 , 380 );
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size( 380 , 380 );
+    video.hide();
     objectDetector = ml5.objectDetector( 'cocossd' , modelLoaded );
     document.getElementById("status").innerHTML = " Status : Detecting Objects " ;
 }
 
 function draw()
 {
-    image( img , 0 ,0 , 640 , 420 );
+    image( video , 0 ,0 , 380 , 380 );
+
+    
+        r = random(255);
+        g = random(255);
+        b = random(255);
+
+      objectDetector.detect(video ,gotResult);
 
     if (status !="")
     {
+
         for(i = 0 ; i < objects.length ; i++)
-        {
-            document.getElementById("status").innerHTML = " Status : Object Detected  " ;
+            {
+                document.getElementById("status").innerHTML = " Status : Object Detected  " ;
+                document.getElementById("number_of_objects").innerHTML = " Number of objects detected are  : " + objetcs.length ;
 
-            fill("#FF0000");
-            precent = floor ( objects[i].confidence * 100 ) ;
-            text( object[i].label + " " + precent + " % " , object[i].x , object[i].y );
-            noFill();
-            stroke("#FF0000");
-            rect (  object[i].x , object[i].y , object[i].width , object[i].height );
 
-        }
+                 fill( r , g , b );
+                 precent = floor ( objects[i].confidence * 100 ) ;
+                 text( object[i].label + " " + precent + " % " , object[i].x , object[i].y );
+                 oFill();
+                 stroke( r , g , b );
+                 rect (  object[i].x , object[i].y , object[i].width , object[i].height );
+
+            }
     }
 
     fill("#FF0000");
@@ -52,7 +65,7 @@ function modelLoaded()
 {
     console.log( " Model Loaded ! " )
     status = true ;
-    objectDetector.detect( img , gotResult);
+    objectDetector.detect( video , gotResult);
 }
 
 function gotResult( error , results )
